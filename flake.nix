27e4dc5db9b74829a6f4b9734bb642cdeef681f5
HeapUnderflow <heapunderflow@outlook.com>
2022-07-21 09:51:50 +0200

    more nix changes

--- flake.nix
+++ flake.nix
@@ -4,10 +4,11 @@
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nix-community/naersk";
     flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -19,23 +20,59 @@
           extensions = [ "clippy" "rustfmt" "rust-src" "rust-analyzer-preview" ];
           targets = [ "x86_64-pc-windows-gnu"];
         });
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            openssl
-            pkgconfig
-            exa
-            fd
-            toolchain
-            valgrind
-            massif-visualizer
-          ];
 
-          shellHook = ''
+        naersk-lib = naersk.lib."${system}".override {
+          rustc = toolchain;
+          cargo = toolchain;
+        };
+
+        _buildInputs = with pkgs; [
+          openssl
+          pkgconfig
+          exa
+          fd
+          toolchain
+          valgrind
+          massif-visualizer
+
+          # deps for eframe
+          glib
+          pango
+          gdk-pixbuf
+          atk
+          gtk3
+        ];
+
+        _nativeBuildInputs = with pkgs; [ wrapGAppsHook ];
+
+        proton_ovr = builtins.readFile ./proton-steam-comptime.txt;
+      in
+        rec {
+          packages.kspacker = naersk-lib.buildPackage {
+            pname = "kspacker";
+            root = ./.;
+            buildInputs = _buildInputs;
+            nativeBuildInputs = _nativeBuildInputs;
+            cargoBuildOptions = options: options ++ ["--features" "proton-steam-comptime"];
+            PROTON_PATH_OVR = proton_ovr;
+          };
+          defaultPackage = packages.kspacker;
+
+          apps.kspacker = flake-utils.lib.mkApp {
+            drv = packages.kspacker;
+          };
+          defaultApp = apps.kspacker;
+
+          devShell = pkgs.mkShell {
+            buildInputs = _buildInputs;
+            nativeBuildInputs = _nativeBuildInputs;
+
+            PROTON_PATH_OVR = proton_ovr;
+
+            shellHook = ''
             echo "Loaded devshell"
           '';
-        };
-      }
+          };
+        }
     );
 }
